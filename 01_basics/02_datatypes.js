fccf18d176cfb81a03c92c2e8196593e4380d859
MindSmith53 <work.areeb01@gmail.com>
2024-12-31 17:36:52 +0000

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,21 @@
+'use strict' // treat all JS Code as newer version
+
+// alert(3 + 3) // we are using nodejs, not browser
+
+// code readability should be as high as possible
+
+/* Types of datatypes:-
+1. Number => 2 to the power 53
+2. bigint
+3. string
+4. boolean
+5. null => standalone value
+6. undefined =>
+7. symbol => unique
+*/
+
+// objects
+let age = 25;
+console.log(typeof age);
+console.log(typeof null);
+console.log(typeof undefined);
